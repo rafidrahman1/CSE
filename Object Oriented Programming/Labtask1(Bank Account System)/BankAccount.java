2842fafcdc211fc8ed4f652818fa60d5a1bf3dcc
rafidrahman1 <rafid0001@gmail.com>
2023-06-13 01:17:36 +0600

    s

---
+++ Object Oriented Programming/Labtask1(Bank Account System)/BankAccount.java
@@ -0,0 +1,36 @@
+public abstract class BankAccount {
+    private int accountNumber;
+    private String accountName;
+    double balance;
+
+    public BankAccount(int accountNumber, String accountName) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = 0.0;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: $" + amount);
+    }
+
+    public abstract void withdraw(double amount);
+
+    public void displayAccountInfo() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Name: " + accountName);
+        System.out.println("Balance: $" + balance);
+    }
+}
