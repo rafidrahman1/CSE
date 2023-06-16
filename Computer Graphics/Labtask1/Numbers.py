2788ebe94b51106769479e69b64050fcd21f5a1e
rafidrahman1 <rafid0001@gmail.com>
2023-06-16 23:40:53 +0600

    Numbers method file

---
+++ Computer Graphics/Labtask1/Numbers.py
@@ -0,0 +1,250 @@
+
+def zero(a,b):
+    glBegin(GL_LINES)
+    glColor3f(1.0, 0.0, 1.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical    
+    glVertex2f(a, b)
+    glVertex2f(a, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    glVertex2f(s, b)
+    glVertex2f(s, x)
+    
+    glVertex2f(s, z)
+    glVertex2f(s, b)
+
+    #horizontal
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+
+    glEnd()
+
+
+def one(a,b):
+    glBegin(GL_LINES)
+    glColor3f(1.0, 1.0, 1.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical
+    glVertex2f(a, b)
+    glVertex2f(a, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    glEnd()
+
+def two(a,b):
+    glBegin(GL_LINES)
+    glColor3f(1.0, 0.0, 0.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical
+    glVertex2f(a, b)
+    glVertex2f(a, x)
+
+    glVertex2f(s, z)
+    glVertex2f(s, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+   
+    glEnd()
+
+def three(a,b):
+    glBegin(GL_LINES)
+    glColor3f(0.0, 1.0, 0.0)
+    x=b+20
+    z=b-20
+    s=a-20
+
+    #verticle
+    glVertex2f(a, b) 
+    glVertex2f(a, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+   
+    glEnd()    
+
+def four(a,b):
+    glBegin(GL_LINES)
+    glColor3f(0.0, 0.0, 1.0)
+    x=b+20
+    z=b-20
+    s=a-20
+
+    #verticle
+    glVertex2f(a, b) 
+    glVertex2f(a, x)
+
+    glVertex2f(s, b)
+    glVertex2f(s, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+   
+    glEnd()  
+
+def five(a,b):
+    glBegin(GL_LINES)
+    glColor3f(1.0, 1.0, 0.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical
+    glVertex2f(s, b)
+    glVertex2f(s, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+   
+    glEnd()    
+
+def six(a,b):
+    glBegin(GL_LINES)
+    glColor3f(0.0, 1.0, 1.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical
+    glVertex2f(s, b)
+    glVertex2f(s, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    glVertex2f(s, z)
+    glVertex2f(s, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+   
+    glEnd()  
+
+def seven(a,b):
+    glBegin(GL_LINES)
+    glColor3f(1.0, 0.0, 1.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical
+    glVertex2f(a, b)
+    glVertex2f(a, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+ 
+    #horizontal
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+   
+    glEnd()
+
+def eight(a,b):
+    glBegin(GL_LINES)
+    glColor3f(1.0, 0.0, 0.0)
+    x=b+20
+    z=b-20
+    s=a-20
+    #vertical    
+    glVertex2f(a, b)
+    glVertex2f(a, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    glVertex2f(s, b)
+    glVertex2f(s, x)
+    
+    glVertex2f(s, z)
+    glVertex2f(s, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+
+    glEnd()
+
+def nine(a,b):
+    glBegin(GL_LINES)
+    glColor3f(0.0, 1.0, 1.0)
+    x=b+20
+    z=b-20
+    s=a-20
+
+    #verticle
+    glVertex2f(a, b) 
+    glVertex2f(a, x)
+
+    glVertex2f(s, b)
+    glVertex2f(s, x)
+
+    glVertex2f(a, z)
+    glVertex2f(a, b)
+
+    #horizontal
+    glVertex2f(s,b)
+    glVertex2f(a,b)
+
+    glVertex2f(s,z)
+    glVertex2f(a,z)
+    
+    glVertex2f(s,x)
+    glVertex2f(a,x)
+   
+    glEnd()  
