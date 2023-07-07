a3440044eeb500b62e14319071b86200d68dd75f
rafidrahman1 <rafid0001@gmail.com>
2023-07-08 04:53:15 +0600

    Done

--- CS couses/Computer Graphics/Labtasks/LabTask2.py
+++ CS couses/Computer Graphics/Labtasks/LabTask2.py
@@ -1,11 +1,15 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-from MidPointNumbers import*
+from MidPointNumbers import *
+import array as arr
 
+print("Enter your ID: ")
+x = input()
+arr = [int(x) for x in str(x)]
 
 def iterate():
-    glViewport(0, 0, 1000, 1000)#zoom
+    glViewport(0, 0, 9000, 9000)#zoom
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
@@ -23,9 +27,40 @@ def showScreen():
     # midpoint_line_algorithm(5, 10, 5, 5)
     # midpoint_line_algorithm(5, 5, 10, 5) 
     # midpoint_line_algorithm(5, 10, 5, 15)  
-    two(5,10,15)
+    a=15
+    b=20
+    c=25
+    
+    j=len(arr)-2
 
-     
+
+    for i in range(2):
+        if arr[j]==0:
+           zero(a,b,c)
+        elif arr[j]==1:
+            one(a,b,c) 
+        elif arr[j]==2:
+            two(a,b,c)     
+        elif arr[j]==3:
+            three(a,b,c)
+        elif arr[j]==4:
+            four(a,b,c)
+        elif arr[j]==5:
+            five(a,b,c)
+        elif arr[j]==6:
+            six(a,b,c)
+        elif arr[j]==7:
+            seven(a,b,c)
+        elif arr[j]==8:
+            eight(a,b,c)
+        else:
+            nine(a,b,c)      
+
+        a+=7
+        b+=7
+        c+=7
+        j+=1
+        
 
     glutSwapBuffers()
 
