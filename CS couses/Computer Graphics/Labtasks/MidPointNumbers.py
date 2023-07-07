90d81ccf9a8885a4ee2b2251b40fdd0272bc7721
rafidrahman1 <rafid0001@gmail.com>
2023-07-08 04:53:24 +0600

    DOne

--- CS couses/Computer Graphics/Labtasks/MidPointNumbers.py
+++ CS couses/Computer Graphics/Labtasks/MidPointNumbers.py
@@ -1,8 +1,10 @@
 from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
 
 def draw_points(x, y):
-    glPointSize(2) #pixel size. by default 1 thake
+    glPointSize(20) #pixel size. by default 1 thake
     glBegin(GL_POINTS)
     glColor3f(1, 0, 1) #color
     glVertex2f(x,y) #jekhane show korbe pixel, Cordinates
@@ -27,224 +29,78 @@ def midpoint_line_algorithm(x0, y0, x1, y1):
 
     draw_points(x0, y0)
 
-# def zero(a,b,c):
-#     glBegin(GL_POINTS)
-#     glColor3f(1.0, 0.0, 1.0)
+def zero(a,b,c):
+    midpoint_line_algorithm(b, b, b, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(b, c, a, c)
+    midpoint_line_algorithm(a, b, a, a)
+    midpoint_line_algorithm(a, a, b, a) 
+    midpoint_line_algorithm(a, b, a, c)  
+
+def one(a,b,c):
+    midpoint_line_algorithm(a, b, a, c)
+    midpoint_line_algorithm(a, a, a, b)
     
-#     midpoint_line_algorithm(10, 10, 10, 15)
-#     midpoint_line_algorithm(10, 5, 10, 10)
-#     midpoint_line_algorithm(10, 15, 5, 15)
-#     midpoint_line_algorithm(5, 10, 5, 5)
-#     midpoint_line_algorithm(5, 5, 10, 5) 
-#     midpoint_line_algorithm(5, 10, 5, 15)  
 
-#     glEnd()
-
-'''
-def one(a,b):
-    glBegin(GL_LINES)
-    glColor3f(1.0, 1.0, 1.0)
-    x=b+20
-    z=b-20
-    s=a-20
-    #vertical
-    glVertex2f(a, b)
-    glVertex2f(a, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
-
-    glEnd()
-'''
 def two(a,b,c):
-    glBegin(GL_POINTS)
-    glColor3f(1.0, 0.0, 0.0)
-    #vertical
+
     midpoint_line_algorithm(a, b, b, b)
     midpoint_line_algorithm(b, c, a, c)
+    midpoint_line_algorithm(b, b, b, c)
     midpoint_line_algorithm(a, b, a, a)
     midpoint_line_algorithm(a, a, b, a)     
    
-    glEnd()
-'''
-def three(a,b):
-    glBegin(GL_LINES)
-    glColor3f(0.0, 1.0, 0.0)
-    x=b+20
-    z=b-20
-    s=a-20
-
-    #verticle
-    glVertex2f(a, b) 
-    glVertex2f(a, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
-
-    #horizontal
-    glVertex2f(s,b)
-    glVertex2f(a,b)
-
-    glVertex2f(s,z)
-    glVertex2f(a,z)
+def three(a,b,c):
     
-    glVertex2f(s,x)
-    glVertex2f(a,x)
-   
-    glEnd()    
+    midpoint_line_algorithm(a, b, b, b)
+    midpoint_line_algorithm(b, c, a, c)
+    midpoint_line_algorithm(b, b, b, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(a, a, b, a)   
 
-def four(a,b):
-    glBegin(GL_LINES)
-    glColor3f(0.0, 0.0, 1.0)
-    x=b+20
-    z=b-20
-    s=a-20
-
-    #verticle
-    glVertex2f(a, b) 
-    glVertex2f(a, x)
-
-    glVertex2f(s, b)
-    glVertex2f(s, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
-
-    #horizontal
-    glVertex2f(s,b)
-    glVertex2f(a,b)
-   
-    glEnd()  
-
-def five(a,b):
-    glBegin(GL_LINES)
-    glColor3f(1.0, 1.0, 0.0)
-    x=b+20
-    z=b-20
-    s=a-20
-    #vertical
-    glVertex2f(s, b)
-    glVertex2f(s, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
-
-    #horizontal
-    glVertex2f(s,b)
-    glVertex2f(a,b)
-
-    glVertex2f(s,z)
-    glVertex2f(a,z)
+def four(a,b,c):
     
-    glVertex2f(s,x)
-    glVertex2f(a,x)
-   
-    glEnd()    
+    midpoint_line_algorithm(a, b, b, b)
+    midpoint_line_algorithm(b, b, b, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(a, b, a, c)  
 
-def six(a,b):
-    glBegin(GL_LINES)
-    glColor3f(0.0, 1.0, 1.0)
-    x=b+20
-    z=b-20
-    s=a-20
-    #vertical
-    glVertex2f(s, b)
-    glVertex2f(s, x)
+def five(a,b,c):
 
-    glVertex2f(a, z)
-    glVertex2f(a, b)
+    midpoint_line_algorithm(a, b, b, b)
+    midpoint_line_algorithm(b, c, a, c)
+    midpoint_line_algorithm(a, b, a, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(a, a, b, a)    
 
-    glVertex2f(s, z)
-    glVertex2f(s, b)
+def six(a,b,c):
 
-    #horizontal
-    glVertex2f(s,b)
-    glVertex2f(a,b)
+    midpoint_line_algorithm(a, b, b, b)
+    midpoint_line_algorithm(a, a, a, b)
+    midpoint_line_algorithm(b, c, a, c)
+    midpoint_line_algorithm(a, b, a, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(a, a, b, a)     
 
-    glVertex2f(s,z)
-    glVertex2f(a,z)
+def seven(a,b,c):
+
+    midpoint_line_algorithm(b, b, b, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(b, c, a, c)
     
-    glVertex2f(s,x)
-    glVertex2f(a,x)
-   
-    glEnd()  
+def eight(a,b,c):
+    midpoint_line_algorithm(b, b, b, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(b, c, a, c)
+    midpoint_line_algorithm(a, b, a, a)
+    midpoint_line_algorithm(a, a, b, a) 
+    midpoint_line_algorithm(a, b, a, c)
+    midpoint_line_algorithm(a, b, b, b) 
 
-def seven(a,b):
-    glBegin(GL_LINES)
-    glColor3f(1.0, 0.0, 1.0)
-    x=b+20
-    z=b-20
-    s=a-20
-    #vertical
-    glVertex2f(a, b)
-    glVertex2f(a, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
- 
-    #horizontal
-    glVertex2f(s,x)
-    glVertex2f(a,x)
-   
-    glEnd()
-
-def eight(a,b):
-    glBegin(GL_LINES)
-    glColor3f(1.0, 0.0, 0.0)
-    x=b+20
-    z=b-20
-    s=a-20
-    #vertical    
-    glVertex2f(a, b)
-    glVertex2f(a, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
-
-    glVertex2f(s, b)
-    glVertex2f(s, x)
+def nine(a,b,c):
     
-    glVertex2f(s, z)
-    glVertex2f(s, b)
-
-    #horizontal
-    glVertex2f(s,b)
-    glVertex2f(a,b)
-
-    glVertex2f(s,z)
-    glVertex2f(a,z)
-    
-    glVertex2f(s,x)
-    glVertex2f(a,x)
-
-    glEnd()
-
-def nine(a,b):
-    glBegin(GL_LINES)
-    glColor3f(0.0, 1.0, 1.0)
-    x=b+20
-    z=b-20
-    s=a-20
-
-    #verticle
-    glVertex2f(a, b) 
-    glVertex2f(a, x)
-
-    glVertex2f(s, b)
-    glVertex2f(s, x)
-
-    glVertex2f(a, z)
-    glVertex2f(a, b)
-
-    #horizontal
-    glVertex2f(s,b)
-    glVertex2f(a,b)
-
-    glVertex2f(s,z)
-    glVertex2f(a,z)
-    
-    glVertex2f(s,x)
-    glVertex2f(a,x)
-  
-    glEnd()  
-    ''' 
+    midpoint_line_algorithm(a, b, b, b)
+    midpoint_line_algorithm(a, c, b, c)
+    midpoint_line_algorithm(b, b, b, c)
+    midpoint_line_algorithm(b, a, b, b)
+    midpoint_line_algorithm(a, b, a, c)         
