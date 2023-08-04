2f56ce70619e06be2934b48697ad754411d9863b
rafidrahman1 <rafid0001@gmail.com>
2023-08-04 12:54:39 +0600

    New springboot project

---
+++ Projects/Local Transport Managment System/localtpmg/src/main/java/com/example/localtpmg/LocaltpmgApplication.java
@@ -0,0 +1,13 @@
+package com.example.localtpmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaltpmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaltpmgApplication.class, args);
+	}
+
+}
---
+++ Projects/Local Transport Managment System/localtpmg/src/test/java/com/example/localtpmg/LocaltpmgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localtpmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaltpmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
