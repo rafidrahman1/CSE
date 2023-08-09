9041d47ab773426674b8b384b2c409c0d1e8343c
rafidrahman1 <rafid0001@gmail.com>
2023-08-09 17:18:31 +0600

    Group project update

--- CS couses/Computer Graphics/Labtasks/GroupProject.py
+++ CS couses/Computer Graphics/Labtasks/GroupProject.py
@@ -1,6 +1,57 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-from Numbers import *
+from MidPointCircle import *
+from MidPointNumbers import *
+from Stage import *
 import random
 import array as arr
+
+
+# print("Enter your ID: ")
+# x = input()
+# arr = [int(x) for x in str(x)]
+
+def iterate():
+    glViewport(0, 0, 2000, 2000)#zoom
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+ 
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    #Natai
+    MidpointLine(40, 20, 40, 50)
+    MidpointLine(20, 35, 40, 35)
+    MidpointLine(135, 20, 135, 50)
+    MidpointLine(135, 35, 155, 35)
+    x=25
+    for i in range(21):
+       MidpointLine(40, x, 135, x)
+       x+=1
+    #Natai er Suta.
+    Stages(4)
+
+    
+
+
+    
+    glutSwapBuffers()
+
+
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(700, 700) #window size
+glutInitWindowPosition(700, 100)#window position
+window = glutCreateWindow(b"500 taka vangti lagbe") #window name
+glutDisplayFunc(showScreen)
+glutMainLoop()
+
+
+
---
+++ CS couses/Computer Graphics/Labtasks/Stage.py
@@ -0,0 +1,28 @@
+from MidPointCircle import *
+
+def Stages(x):
+    if(x==1):
+       ran=37
+    elif x==2:
+       ran=20  
+    elif x==3:
+       ran=10
+    else:
+       ran=0   
+    
+    r=11 #radius
+    #color
+    a=1
+    b=0
+    c=0
+    #centre
+    c_x=88
+    c_xn=88
+    c_y=35    
+    for i in range(ran):
+      MidpointCircle(r, c_x, c_y,a,b,c)
+      c_x+=1
+    for i in range(ran):
+      MidpointCircle(r, c_xn, c_y,a,b,c)
+      c_xn-=1 
+          
