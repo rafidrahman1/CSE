d7a83418f72ea3e4f98c1964198c365f32a46feb
rafidrahman1 <rafid0001@gmail.com>
2023-08-19 01:00:47 +0600

    update

--- CS couses/Computer Graphics/Labtasks/GroupProject/Reel.py
+++ CS couses/Computer Graphics/Labtasks/GroupProject/Reel.py
@@ -1,12 +1,14 @@
 from MidPointLine import *
 def Reel():
-    #Natai border
-    MidPointLine(40, 20, 40, 50, 0, 1, 1, 10)
-    MidPointLine(20, 35, 40, 35, 0, 1, 1, 10)
-    MidPointLine(135, 20, 135, 50, 0, 1, 1, 10)
-    MidPointLine(135, 35, 155, 35, 0, 1, 1, 10)
-    x=25
     #Natai body
+    x=25
     for i in range(21):
        MidPointLine(40, x, 135, x, 1, 1, 1, 5)
        x+=1
+    #Natai border
+    MidPointLine(40, 20, 40, 50, 1, 1, 0, 10)
+    MidPointLine(20, 35, 40, 35, 1, 1, 0, 10)
+    MidPointLine(135, 20, 135, 50, 1, 1, 0, 10)
+    MidPointLine(135, 35, 155, 35,  1, 1, 0, 10)
+    
+    
